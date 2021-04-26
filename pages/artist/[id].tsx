27d21839c2a516371dc0de4b84b9c8ca2acbb610
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 21:37:06 +0000

    add information about artist

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -1,15 +1,81 @@
-import { Heading } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/avatar";
+import { Button } from "@chakra-ui/button";
+import { Box, Container, Heading, Stack, Text, Wrap } from "@chakra-ui/layout";
+import { Tag } from "@chakra-ui/tag";
+import { Instagram, TikTok, Twitch, Twitter, YouTube } from "@components/icon";
 import DefaultLayout from "@components/layout";
 import { GetServerSideProps } from 'next'
+import { useColorModeValue as mode } from "@chakra-ui/color-mode";
 import { NextSeo } from "next-seo";
 
 export default function Artist({ artist }) {
-  const { name } = artist;
+  const { name, genres, avatarUrl, channels } = artist;
 
   return (
     <>
       <NextSeo title={name} />
-      <Heading>{name}</Heading>
+      <Box as="section" mt="10">
+        <Container maxW="container.xl">
+          <Box rounded="lg" shadow="base" bg={mode("white", "purple.800")} p="8">
+            <Stack spacing="5">
+              <Stack direction="row" align="center" spacing="5">
+                <Avatar size="xl" src={avatarUrl} />
+                <Stack justify="center" spacing="2">
+                  <Heading>{name}</Heading>
+                  {genres && (
+                    <Stack direction="row">
+                      {genres.map(genre => (
+                        <Tag colorScheme="purple">{genre}</Tag>
+                      ))}
+                    </Stack>
+                  )}
+                </Stack>
+              </Stack>
+              <Wrap>
+                {channels.map((channel) => {
+                  switch (channel.platform) {
+                    case "youtube": {
+                      return (
+                        <Button colorScheme="red" variant="ghost" leftIcon={<YouTube />}>
+                          {channel.name}
+                        </Button>
+                      )
+                    }
+                    case "instagram": {
+                      return (
+                        <Button colorScheme="purple" variant="ghost" leftIcon={<Instagram />}>
+                          {channel.name}
+                        </Button>
+                      )
+                    }
+                    case "tiktok": {
+                      return (
+                        <Button colorScheme="pink" variant="ghost" leftIcon={<TikTok />}>
+                          {channel.name}
+                        </Button>
+                      )
+                    }
+                    case "twitter": {
+                      return (
+                        <Button colorScheme="blue" variant="ghost" leftIcon={<Twitter />}>
+                          {channel.name}
+                        </Button>
+                      )
+                    }
+                    case "twitch": {
+                      return (
+                        <Button colorScheme="purple" variant="ghost" leftIcon={<Twitch />}>
+                          {channel.name}
+                        </Button>
+                      )
+                    }
+                  }
+                })}
+              </Wrap>
+            </Stack>
+          </Box>
+        </Container>
+      </Box>
     </>
   )
 }
@@ -18,7 +84,10 @@ Artist.Layout = DefaultLayout;
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { id: artistId } = params;
-  const artist = await fetch(`https://api.nindo.de/artist/${artistId}`).then(res => res.json())
+  const data = await fetch(`https://api.nindo.de/artist/${artistId}`).then(res => res.json())
+  const { name, avatar, _genres, _channels } = data;
+
+  const artist = { name, avatarUrl: avatar, genres: _genres.map((genre) => genre.name), channels: _channels }
 
   return {
     props: {

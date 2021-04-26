9ad63b7537109e345a833d0a984d6e5107b04e03
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 13:07:09 +0000

    style footer

--- components/core/Footer/Footer.tsx
+++ components/core/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+import { Box, Stack, Text, Link } from "@chakra-ui/layout";
 import { FC } from "react";
 
 const Footer: FC = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <h1>Footer</h1>
+    <Box as="footer" color="gray.700" py="24">
+      <Stack align="center" spacing="5">
+        <Stack direction="row" spacing="8">
+          <Link href="/kontakt">Kontakt</Link>
+          <Link href="/impressum">Impressum</Link>
+          <Link href="/datenschutz">Datenschutz</Link>
+          <Link href="/hilfe">Hilfe</Link>
+        </Stack>
+        <Text color="gray.500">&copy; {year} Nindo GmbH</Text>
+      </Stack>
+    </Box>
   )
 }
 

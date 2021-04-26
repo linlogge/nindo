d0fc5fb846d3ca50c4c11cadd21e6cec4235b57b
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 18:33:37 +0000

    add fallback

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from "@components/layout"
+
+export default function PageNotFound() {
+  return (
+    <h1>Seite konnte nicht gefunden werden</h1>
+  )
+}
+
+PageNotFound.Layout = DefaultLayout;

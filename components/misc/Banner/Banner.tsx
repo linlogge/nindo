f8861207f7280b3041eabbbbeec1eee122cf4d70
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 13:48:56 +0000

    increase rounded corner size

--- components/misc/Banner/Banner.tsx
+++ components/misc/Banner/Banner.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 const Banner: FC = () => {
   return (
-    <Box rounded="base" bg="purple.500" p="10">
+    <Box rounded="md" bg="purple.500" p="10">
       <Stack align="center" spacing="5">
         <Text color="white">Für Agenturen, Management & Brands <Badge colorScheme="purple">Neu</Badge></Text>
         <Heading color="white" textAlign="center" size="2xl"><Text as="span" color="purple.200">Nindo</Text> Professional</Heading>

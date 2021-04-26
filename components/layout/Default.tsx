de0bfcfa59e48c7c47369d48dfc75dada9b99c3e
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 11:37:24 +0000

    add footer to layout

---
+++ components/layout/Default.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Header, Footer } from "@components/core";
+
+const DefaultLayout: FC = ({ children }) => {
+  return (
+    <>
+      <Header />
+      { children}
+      <Footer />
+    </>
+  )
+}
+
+export default DefaultLayout;

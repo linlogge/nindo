5064bfab1c94cc3e5f66ae2713c01217e97b4476
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 11:37:37 +0000

    implement chakra ui and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { FC } from "react";
+
+const Noop: FC = ({ children }) => <>{children}</>;
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const Layout: FC = (Component as any).Layout || Noop;
+
+  return (
+    <>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default App

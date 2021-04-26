14745a9e2c8921256aad2df1eb88fda5bf64a4bd
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 16:52:23 +0000

    add header to page

--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,7 +1,16 @@
+import { SimpleGrid, Box, Container } from "@chakra-ui/layout";
+import { Header } from "@components/core";
 import DefaultLayout from "@components/layout";
 
 export default function YouTube() {
-  return <></>;
+  return (
+    <>
+      <Header>YouTube</Header>
+      <Box as="section">
+        <Container maxW="container.xl"></Container>
+      </Box>
+    </>
+  );
 }
 
 YouTube.Layout = DefaultLayout;

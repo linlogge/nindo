2725c5918ce376daf2988b0d6a4a308de7be8731
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 12:31:26 +0000

    rename header

--- components/core/Navigation/Navigation.tsx
+++ components/core/Navigation/Navigation.tsx
@@ -1,10 +1,11 @@
-import { Button, IconButton } from "@chakra-ui/button";
 import { Box, Container, Flex, Heading, Stack } from "@chakra-ui/layout";
 import { FC } from "react";
 import { AiFillYoutube as YouTube, AiFillInstagram as Instagram, AiFillFire as Fire } from "react-icons/ai";
 import { SiTiktok as TikTok, SiTwitter as Twitter, SiTwitch as Twitch } from "react-icons/si";
 import { RiCoupon2Fill as Coupon } from "react-icons/ri";
 import { Input } from "@chakra-ui/input";
+import NavChartsItem from "./NavChartsItem";
+import NavItem from "./NavItem";
 
 const Header: FC = () => {
   return (
@@ -13,15 +14,15 @@ const Header: FC = () => {
         <Flex justify="space-between" align="center">
           <Heading color="purple.500">Nindo</Heading>
           <Stack direction="row">
-            <IconButton icon={<YouTube />} aria-label="YouTube" variant="ghost" colorScheme="purple" />
-            <IconButton icon={<Instagram />} aria-label="Instagram" variant="ghost" colorScheme="purple" />
-            <IconButton icon={<TikTok />} aria-label="TikTok" variant="ghost" colorScheme="purple" />
-            <IconButton icon={<Twitter />} aria-label="Twitter" variant="ghost" colorScheme="purple" />
-            <IconButton icon={<Twitch />} aria-label="Twitch" variant="ghost" colorScheme="purple" />
+            <NavChartsItem icon={<YouTube />} label="YouTube" href="/youtube" />
+            <NavChartsItem icon={<Instagram />} label="Instagram" href="/instagram" />
+            <NavChartsItem icon={<TikTok />} label="TikTok" href="/tiktok" />
+            <NavChartsItem icon={<Twitter />} label="Twitter" href="/twitter" />
+            <NavChartsItem icon={<Twitch />} label="Twitch" href="/twitch" />
           </Stack>
           <Stack direction="row">
-            <Button leftIcon={<Fire />} variant="ghost" colorScheme="purple">Viral</Button>
-            <Button leftIcon={<Coupon />} variant="ghost" colorScheme="purple">Rabatt</Button>
+            <NavItem icon={<Fire />} href="/viral">Viral</NavItem>
+            <NavItem icon={<Coupon />} href="/viral">Rabatt</NavItem>
           </Stack>
           <Box>
             <Input placeholder="Suchen..." variant="filled" />

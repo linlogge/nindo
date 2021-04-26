5cd0d88e0d15efd1f131eafc6997a231d8f65f3e
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-26 12:31:48 +0000

    outsource nav items

---
+++ components/core/Navigation/NavChartsItem.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { FC, ReactElement } from "react";
+import { IconButton } from "@chakra-ui/button";
+
+interface NavChartsItemProps {
+  href: string;
+  label: string;
+  icon: ReactElement;
+}
+
+const NavChartsItem: FC<NavChartsItemProps> = (props) => {
+  const { href, icon, label } = props;
+
+  return (
+    <Link href={href} passHref>
+      <IconButton as="a" icon={icon} aria-label={label} variant="ghost" colorScheme="purple" />
+    </Link>
+  )
+}
+
+export default NavChartsItem;
---
+++ components/core/Navigation/NavItem.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FC, ReactElement } from "react";
+import { Button } from "@chakra-ui/button";
+
+interface NavItemProps {
+  href: string;
+  icon: ReactElement;
+}
+
+const NavItem: FC<NavItemProps> = (props) => {
+  const { href, icon, children } = props;
+
+  return (
+    <Link href={href} passHref>
+      <Button as="a" leftIcon={icon} variant="ghost" colorScheme="purple">
+        {children}
+      </Button>
+    </Link>
+  )
+}
+
+export default NavItem;

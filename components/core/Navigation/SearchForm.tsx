eccc6aaf330c39c69e66d810e86992f7f7950a70
Nöl <65312133+linlogge@users.noreply.github.com>
2021-04-27 11:02:17 +0000

    add data fetching

--- components/core/Navigation/SearchForm.tsx
+++ components/core/Navigation/SearchForm.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { Input } from "@chakra-ui/input";
+import { LinkOverlay, Text, LinkBox } from "@chakra-ui/layout";
 import {
   Modal,
   ModalOverlay,
@@ -10,19 +11,32 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/modal";
 import { useColorModeValue as mode } from "@chakra-ui/color-mode";
-import { useDisclosure } from "@chakra-ui/react";
+import { Avatar, Stack, useDisclosure } from "@chakra-ui/react";
+import Link from "next/link";
 
 export const SearchForm: FC = () => {
+  const [data, setData] = useState([]);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [searchValue, setSearchValue] = useState("");
   const handleChange = (event) => setSearchValue(event.target.value);
+
   const initialRef = useRef();
   const finalRef = useRef();
 
   useEffect(() => {
+    const fetchData = async () => {
+      const term = encodeURIComponent(searchValue);
+      const res = await fetch(`/api/search/${term}`);
+      const data = await res.json();
+      setData(data);
+    };
     if (searchValue.length > 1) {
+      fetchData();
       onOpen();
     } else {
+      setData([]);
       onClose();
     }
   }, [searchValue]);
@@ -45,6 +59,24 @@ export const SearchForm: FC = () => {
           <ModalBody>
             <Input value={searchValue} onChange={handleChange} size="lg" ref={initialRef} />
           </ModalBody>
+          {data && (
+            <ModalBody>
+              <Stack spacing="0">
+                {data.map((artist, i) => (
+                  <LinkBox py="1">
+                    <Stack direction="row" justify="space-between" align="center" key={i}>
+                      <Avatar src={artist.avatar} />
+                      <Link href={`/artist/${artist.id}`} passHref>
+                        <LinkOverlay onClick={onClose} _hover={{ color: "purple.500" }}>
+                          <Text fontWeight="bold">{artist.name}</Text>
+                        </LinkOverlay>
+                      </Link>
+                    </Stack>
+                  </LinkBox>
+                ))}
+              </Stack>
+            </ModalBody>
+          )}
           <ModalFooter />
         </ModalContent>
       </Modal>
